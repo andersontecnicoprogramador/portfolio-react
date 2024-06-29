fcce42cf08938ed5a0b4468b8882b87c5ae28752
Anderson Moreira <anderson.tecnicoprogramdor@hotmail.com>
2024-06-29 00:09:45 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Header/Footer'
 import Header from './components/Header/Header'
 
 function App() {
@@ -7,6 +8,23 @@ function App() {
   return (
     <>
      <Header />
+     <section className='container'>
+      <div className='apresentacao'>
+
+        <p>
+          Olá,sou <br/>
+          <span>Anderson Moreira</span> <br />
+          Dev Full Stack
+        </p>
+        <button className='btn btn-red'>
+          Saiba mais sobre mim
+        </button>
+      </div>
+      <figure>
+        <img className='img-home' src="/developer-red.svg" alt="Imagem de Home" />
+      </figure>
+     </section>
+     <Footer />
     </>
   )
 }
---
+++ src/components/Header/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return(
+        <footer className={styles.footer}>
+            Feito com ❣️ carinho por Anderson Moreira &copy; 2024
+        </footer>
+    )
+}
+
+export default Footer
